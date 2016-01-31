18b7383a5985cc3d1aca0d4fdd60b01059c32294
masonfox <masonfox22@gmail.com>
2016-01-31 14:04:27 -0500

    basic working model

--- client/templates/home/dropzone.js
+++ client/templates/home/dropzone.js
@@ -1,5 +1,15 @@
 Template.dropzone.events({
   'dropped #dropzone': function(e) {
-    console.log('dropped a file');
+      FS.Utility.eachFile(e, function(file) {
+        var newFile = new FS.File(file);
+        
+        Images.insert(newFile, function (error, fileObj) {
+          if (error) {
+            toastr.error("Upload failed... please try again.");
+          } else {
+            toastr.success('Upload succeeded!');
+          }
+      });
+    });
   }
 });
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Wallaby
+> A simple open source, hi-def image sharing site!
+Wallaby is a base app made from a tutorial built upon by Mason Fox
+
+### Run Meteor
+`$ meteor --settings settings.json`
